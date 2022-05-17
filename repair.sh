cc2d9704d033cdc3e1e60d70b634ef85a8613bc0
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2022-05-17 17:50:49 +0800

    update

--- repair.sh
+++ repair.sh
@@ -7,18 +7,28 @@ echo "Current host is: $HOST"
 DATE=`date +%Y-%m-%d`
 echo $DATE
 DIRECTORY=./save/${DATE}/
+
+if [ ! -d "./save" ]; then
+    mkdir ./save
+fi
+
 if [ ! -d "$DIRECTORY" ]; then
     mkdir ./save/${DATE}/
 fi
 
 ############### Configuration   ##############################
 
-DATA_ROOT='YOUR DATASET'
-
+DATA_ROOT='./dataset' #YOUR DATASET ROOT
+if [ ! -d "$DATA_ROOT" ]; then
+    mkdir $DATA_ROOT
+fi
 #  ------Test Center:  Selection ------
 DATASET='cifar10'
 # DATASET='svhn'
 # DATASET='gtsrb'
+if [ ! -d "./$DATA_ROOT/$DATASET" ]; then
+    mkdir ./$DATA_ROOT/$DATASET
+fi
 
 # default parameters
 weight_decay=5e-4
@@ -27,73 +37,71 @@ lr=0.001
 STEP=100
 ####  ------Test Center:  Selection Version 2, fixed mini-budget ------
 # run testing
-if [ "$function" == "selection" ]; then 
-    for avg_clustersize in 10; do # default=10
-    for p_budget in 1; do # 1, 5, 10
-    for RANDOM_SEED in 10; do # 10, 20, 30
-    for exp in 0; do # 0 represents cut the unlabeled data > tau; -1 represents all unlabeled data are selected.
-    for lam in 100; do # The ratio between labeled and unlabeled data: default=100
-    for MODEL in 'MobileNet'; do # MobileNet, resnet18, ShuffleNetG2
-        for MODEL_NO in 0; do # 0, 1, 2 represents Model A, B, C
-            for SOLUTION in 'TTA'; do
-                for eps in 0.3;do
-                for min_samples in 3; do #default=3
-                    for tao in 0.1; do
-                        clusteralg='hybrid'
-                        fe='mocov3'
 
-                        echo 'tao='$tao
-                        echo $SOLUTION
-                        echo $MODEL_NO
-                        echo $eps
-                        echo $clusteralg
-                        echo $fe
-                        echo $avg_clustersize
+for avg_clustersize in 10; do # default=10
+for p_budget in 1; do # 1, 5, 10
+for RANDOM_SEED in 10; do # 10, 20, 30
+for exp in 0; do # 0 represents cut the unlabeled data > tau; -1 represents all unlabeled data are selected.
+for lam in 100; do # The ratio between labeled and unlabeled data: default=100
+for MODEL in 'MobileNet'; do # MobileNet, resnet18, ShuffleNetG2
+    for MODEL_NO in 0; do # 0, 1, 2 represents Model A, B, C
+        for SOLUTION in 'TTA'; do
+            for eps in 0.3;do
+            for min_samples in 3; do #default=3
+                for tao in 0.1; do
+                    clusteralg='hybrid'
+                    fe='mocov3'
 
-                        MODEL2TEST=${MODEL}
-                        MODEL2TESTPATH=./checkpoint/${DATASET}/ckpt_bias/${MODEL}_${MODEL_NO}_b.t7 
-                        save_path=save/${DATE}/${DATASET}_${MODEL2TEST}_${STEP}
-                        echo 'model to test arch '$MODEL2TEST
-                        echo 'model to test path '$MODEL2TESTPATH
-                        python selection.py \
-                                --dataset $DATASET \
-                                --manualSeed ${RANDOM_SEED} \
-                                --model2test_arch $MODEL2TEST \
-                                --model2test_path $MODEL2TESTPATH \
-                                --model_number $MODEL_NO \
-                                --step ${STEP} \
-                                --p_budget ${p_budget} \
-                                --save_path ${save_path} \
-                                --data_path ${DATA_ROOT} \
-                                --solution ${SOLUTION} \
-                                --retrain_lr ${lr} \
-                                --retrain_weightdecay ${weight_decay} \
-                                --retrain_epoch ${epochs} \
-                                --retrain \
-                                --exp $exp \
-                                --eps $eps \
-                                --lam_ul $lam \
-                                --min_samples $min_samples \
-                                --fe $fe \
-                                --cluster $clusteralg \
-                                --avg_clustersize ${avg_clustersize} \
-                                --tao $tao \
-                                --sel_u_data \
-                                --u_weight \
-                                --auto_tao \
-                                --sel_l_data 
-                    done
-                    done
-                    done
-                    done
+                    echo 'tao='$tao
+                    echo $SOLUTION
+                    echo $MODEL_NO
+                    echo $eps
+                    echo $clusteralg
+                    echo $fe
+                    echo $avg_clustersize
+
+                    MODEL2TEST=${MODEL}
+                    MODEL2TESTPATH=./checkpoint/${DATASET}/ckpt_bias/${MODEL}_${MODEL_NO}_b.t7 
+                    save_path=save/${DATE}/${DATASET}_${MODEL2TEST}_${STEP}
+                    echo 'model to test arch '$MODEL2TEST
+                    echo 'model to test path '$MODEL2TESTPATH
+                    python selection.py \
+                            --dataset $DATASET \
+                            --manualSeed ${RANDOM_SEED} \
+                            --model2test_arch $MODEL2TEST \
+                            --model2test_path $MODEL2TESTPATH \
+                            --model_number $MODEL_NO \
+                            --step ${STEP} \
+                            --p_budget ${p_budget} \
+                            --save_path ${save_path} \
+                            --data_path ${DATA_ROOT} \
+                            --solution ${SOLUTION} \
+                            --retrain_lr ${lr} \
+                            --retrain_weightdecay ${weight_decay} \
+                            --retrain_epoch ${epochs} \
+                            --retrain \
+                            --exp $exp \
+                            --eps $eps \
+                            --lam_ul $lam \
+                            --min_samples $min_samples \
+                            --fe $fe \
+                            --cluster $clusteralg \
+                            --avg_clustersize ${avg_clustersize} \
+                            --tao $tao \
+                            --sel_u_data \
+                            --u_weight \
+                            --auto_tao \
+                            --sel_l_data 
                 done
                 done
                 done
                 done
             done
+            done
+            done
+            done
         done
     done
-
-fi
+done
 
 
